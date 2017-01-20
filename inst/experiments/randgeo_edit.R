f8fdc328f0693258b967e10c8e6d79f9c45d6e9b
timelyportfolio <kent.russell@timelyportfolio.com>
2017-01-20 11:47:06 -0600

    demonstrate how to edit provided geo with Leaflet.Draw

---
+++ inst/experiments/randgeo_edit.R
@@ -0,0 +1,24 @@
+# demonstrate Leaflet.Draw on a layer
+
+# will use the very new randgeo
+# devtools::install_github("ropensci/randgeo")
+
+library(leaflet)
+library(leaflet.extras)
+library(randgeo)  # see install instructions above
+
+leaflet() %>%
+  addTiles() %>%
+  addGeoJSON(
+    geo_polygon(
+      count = 10,
+      bbox = c(50,50,60,60),
+      max_radial_length = 2
+    ),
+    layerId = "randgeo"
+  ) %>%
+  addDrawToolbar(
+    targetLayer = "randgeo",
+    editOptions = editToolbarOptions()
+  ) %>%
+  fitBounds(48,48,62,62)
