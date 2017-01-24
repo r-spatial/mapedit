45b1cd3ac201ad72d79b541bb430655e9a5d5afa
timelyportfolio <kent.russell@timelyportfolio.com>
2017-01-24 09:30:11 -0600

    improve select query example shiny and crosstalk

--- inst/experiments/select_shiny_crosstalk.R
+++ inst/experiments/select_shiny_crosstalk.R
@@ -1,9 +1,14 @@
 library(maps)
 library(sp)
 library(maptools)
-library(purrr)
+library(sf)
+library(dplyr)
+
+library(leaflet)
+library(plotly)
 library(htmltools)
 library(crosstalk)
+library(shiny)
 
 usa <- map("state", fill = TRUE)
 IDs <- sapply(strsplit(usa$names, ":"), function(x) x[1])
@@ -11,15 +16,23 @@ usa <- map2SpatialPolygons(usa, IDs=IDs, proj4string=CRS("+proj=longlat +datum=W
 
 # convert to simple features
 usa_sf <- st_as_sf(usa)
-usa_sf$state <- unique(lapply(usa@polygons,function(x){x@ID}))
+usa_sf$state <- unlist(unique(lapply(usa@polygons,function(x){x@ID})))
+# add area from R builtin data state.area
+usa_sf$area <- left_join(
+  usa_sf,
+  data.frame(
+    state = tolower(state.name),
+    area = state.area,
+    stringsAsFactors = FALSE
+  )
+)$area
 
-# use crosstalk SharedData with sf
-sd <- SharedData$new(usa_sf, key=~state, group="states")
+# check our simple features
+plot(usa_sf)
 
 # map states with leaflet
-lf <- leaflet(sd) %>%
+lf <- leaflet(usa_sf) %>%
   addPolygons(group = ~state)
-
 
 # modify Shiny leaflet click event to include shift and ctrl key
 tgs <- tagList(
@@ -38,7 +51,7 @@ var toggle_opacity = function(group) {
   var opaqueness = 0.2;
   lf.layerManager
     .getLayerGroup(group)
-    .eachLayer(function(x){opaqueness = +$(x._path).css('fillOpacity')})
+    .eachLayer(function(x){opaqueness = Math.round(+$(x._path).css('fillOpacity')*10)/10})
   opaqueness = opaqueness === 0.2 ? 0.6 : 0.2;
   layer.setStyle({fillOpacity:opaqueness});
 };
@@ -60,81 +73,107 @@ $(document).on(
   )
 )
 
-library(shiny)
 # set this up in .GlobalEnv for now
 #   but would be better in a gadget
-selections <- list()
+selections <- data.frame(
+  state = usa_sf$state,
+  selected = FALSE
+)
+
 shinyApp(
   tgs,
   function(input, output){
+    # make sure we start with selected = FALSE
+    selections$selected <<- FALSE
+
     id = "undefined"
-    if(!is.null(lf$elementId)) {
-      id = "lf$elementId"
-    }
     click_evt = paste0(id, "_shape_click")
 
     observeEvent(input[[click_evt]], {
       print(input[[click_evt]])
-      selections <<- c(selections, list(input[[click_evt]]))
+      selections[which(selections$state==input[[click_evt]]$group),"selected"] <<- !selections[which(selections$state==input[[click_evt]]$group),"selected"]
     })
-  })
+  }
+)
 
 
 # ugly hack to try to achieve crosstalk support
 #   crosstalk and leaflet don't support addPolygons
-tgs <- tagList(
-  lf,
-  htmlwidgets::onStaticRenderComplete(
+
+shinyApp(
+  fluidPage(
+    fluidRow(
+      column(6,leafletOutput("leafmap")),
+      column(6,plotlyOutput("plotly"))
+    )
+  ),
+  function(input, output, session) {
+    # use crosstalk SharedData with sf
+    sd <- SharedData$new(usa_sf, key=~state, group="states")
+    output$plotly <- renderPlotly({
+      plot_ly(sd, x=~area, y=~state) %>% add_markers()
+    })
+
+    output$leafmap <- renderLeaflet({
+      leaflet(sd) %>%
+        addPolygons(group = ~state) %>%
+        htmlwidgets::onRender(
 "
-debugger;
-var ct_select = new crosstalk.SelectionHandle('states');
+function(el,x) {
+  var ct_select = new crosstalk.SelectionHandle('states');
 
-ct_select.on('change', function(val){console.log(val)});
+  ct_select.on('change', function(val){console.log(val)});
 
-var lf = HTMLWidgets.find('.leaflet').getMap();
+  var lf = this;
 
-// define our functions for toggling
-function toggle_opacity(group) {
-  // change opacity in a crude way
-  //   assuming user just stuck with the defaults
-  var layer = lf.layerManager.getLayerGroup(group);
-  var opaqueness = 0.2;
-  if(layer.eachLayer) {
-    layer.eachLayer(function(x){opaqueness = +$(x._path).css('fillOpacity')})
-  }
-  opaqueness = opaqueness === 0.2 ? 0.6 : 0.2;
-  layer.setStyle({fillOpacity:opaqueness});
-  return opaqueness;
-};
-
-function toggle_state(group) {
-  var selected = ct_select.value;
-  debugger;
-  if(Array.isArray(selected) && selected.length > 0) {
-    var new_selection = selected.slice();
-    var loc = new_selection.indexOf(group);
-    if(loc >= 0) {
-      new_selection.splice(loc,1);
-      ct_select.set(new_selection);
-    } else {
-      new_selection.push(group);
-      ct_select.set(new_selection);
+  // define our functions for toggling
+  function toggle_opacity(group) {
+    // change opacity in a crude way
+    //   assuming user just stuck with the defaults
+    var layer = lf.layerManager.getLayerGroup(group);
+    var opaqueness = 0.2;
+    if(layer.eachLayer) {
+      layer.eachLayer(function(x){opaqueness = Math.round(+$(x._path).css('fillOpacity')*10)/10})
     }
-  } else {
-    ct_select.set([group]);
-  }
-};
+    opaqueness = opaqueness === 0.2 ? 0.6 : 0.2;
+    layer.setStyle({fillOpacity:opaqueness});
+    return opaqueness;
+  };
 
-// set up click handler on each layer
-lf.eachLayer(function(lyr){
-	if(lyr.on && lyr.groupname) {
-    lyr.on('click',function(e){
-      var group = this.groupname;
-      var selected = toggle_opacity(group) === 0.6;
-      toggle_state(group, selected);
+  function toggle_state(group) {
+    var selected = ct_select.value;
+    if(Array.isArray(selected) && selected.length > 0) {
+      var new_selection = selected.slice();
+      var loc = new_selection.indexOf(group);
+      if(loc >= 0) {
+        new_selection.splice(loc,1);
+        //if(new_selection.length === 0) {
+        //  ct_select.clear();
+        //} else {
+          ct_select.set(new_selection);
+        //}
+      } else {
+        new_selection.push(group);
+        ct_select.set(new_selection);
+      }
+    } else {
+      ct_select.set([group]);
+    }
+  };
+
+  // set up click handler on each layer
+  lf.eachLayer(function(lyr){
+    if(lyr.on && lyr.groupname) {
+      lyr.on('click',function(e){
+        var group = this.groupname;
+        var selected = toggle_opacity(group) === 0.6;
+        toggle_state(group, selected);
+      })
+    }
+  });
+}
+"
+        )
     })
   }
-});
-"
-  )
-) %>% browsable()
+)
