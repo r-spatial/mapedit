24069170eca1ea05e12fc922000b1184425b6883
timelyportfolio <kent.russell@timelyportfolio.com>
2017-03-20 09:36:35 -0500

    very ugly first draft of converting edit_map to sf and in fact convert all geojson to sf/sfc

---
+++ inst/experiments/edit_map_return_sf.R
@@ -0,0 +1,68 @@
+# test geojson conversion functions
+library(sf)
+st_as_sfc.geo_list = function(x, ...) {
+  x = switch(x$type,
+             Point = st_point(x$coordinates),
+             MultiPoint = st_multipoint(x$coordinates),
+             LineString = st_linestring(x$coordinates),
+             MultiLineString = st_multilinestring(x$coordinates),
+             Polygon = st_polygon(x$coordinates),
+             MultiPolygon = st_multipolygon(x$coordinates),
+             GeometryCollection = st_geometrycollection(
+               lapply(x$geometries, function(y) st_as_sfc.geo_list(y)[[1]])),
+             stop("unknown class")
+  )
+  st_sfc(x, crs = st_crs(4326))
+}
+
+st_as_sf.geo_list = function(x, ...) {
+  x = switch(x$type,
+             Feature = st_sf(
+               t(unlist(x$properties)),
+               feature = st_as_sfc(x$geometry)
+             ),
+             stop("should be of type 'Feature'")
+  )
+}
+
+
+fix_geojson_coords <- function(ft) {
+
+  if(ft$geometry$type == "Point") {
+    ft$geometry$coordinates <- unlist(ft$geometry$coordinates)
+  }
+
+  if(ft$geometry$type == "LineString") {
+    ft$geometry$coordinates <- matrix(
+      unlist(ft$geometry$coordinates),
+      ncol = 2,
+      byrow = TRUE
+    )
+  }
+
+  if(!(ft$geometry$type %in% c("Point", "LineString"))) {
+    ft$geometry$coordinates <- list(
+      matrix(
+        unlist(ft$geometry$coordinates),
+        ncol = 2,
+        byrow = TRUE
+      )
+    )
+  }
+
+  ft
+}
+
+convert_geojson_coords <- function(gj) {
+  feats <- lapply(
+    gj,
+    function(ft) {
+      ft <- fix_geojson_coords(ft)
+      st_as_sfc.geo_list(ft$geometry)
+    }
+  )
+
+  st_sf(
+    features = do.call(st_sfc, unlist(feats, recursive=FALSE))
+  )
+}
---
+++ inst/experiments/edit_map_return_sf_tests.R
@@ -0,0 +1,53 @@
+library(geojsonio)
+
+file <- system.file("examples", "norway_maple.kml", package = "geojsonio")
+nwy_map <- geojson_read(as.location(file), what = "list")
+listviewer::jsonedit(nwy_map)
+
+pt <- nwy_map$features[[1]]$geometry
+class(pt) <- "geo_list"
+
+
+p_list <- lapply(list(c(3.2,4), c(3,4.6), c(3.8,4.4)), st_point)
+pt_sfc <- st_sfc(p_list)
+pt_sf <- st_sf(x = c("a", "b", "c"), pt_sfc)
+
+# use geojsonio to convert sf to geojson
+gj_pt_sf <- geojsonio::geojson_list(pt_sf)
+
+# start by trying to convert a single geojson feature to s
+feat <- gj_pt_sf$features[[1]]
+
+# manually translate one geojson feature
+one_feat <- st_sf(
+  feat$properties,
+  st_as_sfc.geo_list(feat$geometry)
+)
+
+
+# now try to convert all of the geojson features
+feats <- lapply(
+  gj_pt_sf$features,
+  function(ft) {
+    st_sf(
+      ft$properties,
+      st_as_sfc.geo_list(ft$geometry)
+    )
+  }
+)
+
+do.call(rbind, feats)
+
+
+nwy_sf <- convert_geojson_coords(nwy_map$features)
+
+library(leaflet)
+library(mapedit)
+# draw something with mapedit::edit_map
+#   and try to convert to sf
+me_gj <- edit_map(leaflet())$finished
+# coordinates from edit_map not in proper form
+#  so added a fix function
+convert_geojson_coords(
+  lapply(me_gj,function(x) list(geometry = x$geometry))
+)
