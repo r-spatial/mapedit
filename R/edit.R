9cdc1fa913dd85890b22ff939a4c61e2b815762b
timelyportfolio <kent.russell@timelyportfolio.com>
2017-05-25 12:31:32 -0500

    add editMap.mapview

--- R/edit.R
+++ R/edit.R
@@ -148,3 +148,15 @@ editMap.leaflet <- function(x = NULL, targetLayerId = NULL, sf = TRUE) {
   )
 }
 
+#' @export
+editMap.mapview <- function(x = NULL, targetLayerId = NULL, sf = TRUE) {
+  stopifnot(!is.null(x), inherits(x, "mapview"), inherits(x@map, "leaflet"))
+
+  stopifnot(
+    requireNamespace("leaflet.extras"),
+    requireNamespace("shiny"),
+    requireNamespace("miniUI")
+  )
+
+  editMap.leaflet(x@map, targetLayerId = targetLayerId, sf = sf)
+}
