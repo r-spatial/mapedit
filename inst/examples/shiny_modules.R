fdfe37bddddc4ecfc4640eb7f7bda2d8e1d5bb7c
timelyportfolio <kent.russell@timelyportfolio.com>
2017-05-31 07:28:45 -0500

    complete first draft of editMap as shiny  module

--- inst/examples/shiny_modules.R
+++ inst/examples/shiny_modules.R
@@ -1,7 +1,10 @@
+\dontrun{
+
 library(mapedit)
 library(mapview)
 library(shiny)
 
+# select as a module
 m = leaflet(breweries91) %>%
   addCircleMarkers(weight = 1, group = ~as.character(1:nrow(breweries91)))
 
@@ -16,6 +19,25 @@ server <- function(input, output, session) {
 }
 shinyApp(ui, server)
 
+# edit as a module
+library(mapedit)
+library(mapview)
+library(shiny)
 
+m = mapview(breweries91)@map
+testsf = NULL
+ui <- tagList(
+  editModUI("test-edit"),
+  h1("What You Draw"),
+  leafletOutput("edited")
+)
+server <- function(input, output, session) {
+  crud <- callModule(editMod, "test-edit", m, "breweries91")
+  output$edited <- renderLeaflet({
+    req(crud()$finished)
+    mapview(crud()$finished)@map
+  })
+}
+shinyApp(ui, server)
 
-
+}
