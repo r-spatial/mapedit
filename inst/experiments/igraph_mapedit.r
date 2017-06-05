dab9021d977dfeafadaa70e38dd76c99226a9b00
timelyportfolio <kent.russell@timelyportfolio.com>
2017-06-04 21:37:00 -0500

    fix igraph layout experiment to work with sf and modules

--- inst/experiments/igraph_mapedit.r
+++ inst/experiments/igraph_mapedit.r
@@ -1,6 +1,8 @@
 library(igraph)
-library(leaflet)
+library(mapview)
 library(mapedit)
+library(sf)
+
 
 karate <- graph.famous("Zachary")
 igrf_layout <- layout.auto(karate)
@@ -8,9 +10,20 @@ igrf_layout <- layout.auto(karate)
 # see a default plot with our layout
 plot(karate, layout=igrf_layout)
 
+geom <- st_cast(
+  st_sfc(
+    st_multipoint(igrf_layout)
+  ),
+  "POINT"
+)
+igrf_sf <- st_sf(geom)
+
+# plot with sf to confirm tranform occurred correctly
+plot(igrf_sf)
+
 # plot with leaflet
 lf <- leaflet(
-  igrf_layout,
+  igrf_sf,
   options = leafletOptions(
     crs = leafletCRS(crsClass = "L.CRS.Simple")
   )
@@ -24,50 +37,48 @@ new_layout <- lf %>%
 #   but we will find a much easier way in mapedit
 #   eventually
 library(shiny)
-shinyApp(
-  htmlwidgets::onRender(
-    lf,
+app <- shinyApp(
+  ui = editModUI("mapeditor"),
+  server = function(input, output, session) {
+    edits = callModule(
+      editMod,
+      "mapeditor",
+      htmlwidgets::onRender(
+        lf,
 "
 function(el,x) {
   var lf = this;
   setTimeout(
     function(){
       Shiny.onInputChange(
-        'getpoints',
-        Object.keys(lf.layerManager.getLayerGroup('network')._layers)
+      'getpoints',
+      Object.keys(lf.layerManager._byGroup.network)
       )
     },
     500
   )
 }
 "
-  ),
-  function(input, output) {
-    observeEvent(input$getpoints, {leafids <<- input$getpoints})
+      ),
+      targetLayerId = "network"
+    )
+
+    observeEvent(input$getpoints, {stopApp(input$getpoints)})
   }
 )
 
+leafids <- runApp(app)
+
 # now use our hacked method of id retrieval to identify points
 library(dplyr)
-library(purrr)
 
-layout_df <- data.frame(igrf_layout) %>%
-  mutate(leafid = leafids) %>%
-  left_join(
-    map_df(
-      new_layout$edited[[1]]$features,
-      ~data.frame(
-        "leafid" = as.character(.x$properties["_leaflet_id"]),
-        "newx" = .x$geometry$coordinates[[1]],
-        "newy" = .x$geometry$coordinates[[2]],
-        stringsAsFactors = FALSE
-      )
-    ) %>%
-      set_names(c("leafid", "newx", "newy"))
-  )
+layout_sf <- igrf_sf %>%
+  mutate(leafid = as.numeric(leafids)) %>%
+  {
+    .[which(.$leafid %in% new_layout$edited$X_leaflet_id),]$geom <- new_layout$edited$feature
+    .
+  }
 
-layout_df <- layout_df %>%
-  mutate(X1 = ifelse(is.na(newx),X1,newx)) %>%
-  mutate(X2 = ifelse(is.na(newy), X2, newy))
+mapview(layout_sf)
 
 plot(karate, layout=data.matrix(layout_df[,1:2]))
