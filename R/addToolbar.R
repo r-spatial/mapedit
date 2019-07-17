bb5e7c90db9a2550be9c731d5b70edb48b5f927c
timelyportfolio <kent.russell@timelyportfolio.com>
2019-07-16 21:08:54 -0500

    fix mess up in 469f6205a1462e30040f7f29b7a6602277dd6e67 that was messed up fix for 6b206f8ddec62ca8b1bd5fdb12f5c0a8c8925d40

--- R/addToolbar.R
+++ R/addToolbar.R
@@ -11,7 +11,7 @@
 ##'     \code{leafpm::addPmToolbar}.
 processOpts <- function(fun, args) {
     ## Account for special meaning of `FALSE` as arg in leaflet.extras
-    if(!is.null(args) && !isTRUE(args)) {
+    if(identical(args, FALSE)) {
         return(FALSE)
     } else {
         return(do.call(fun, args))
