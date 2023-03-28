74f986b918df4bf60b48d15550a4abaa5d358e68
Eli Pousson <eli.pousson@gmail.com>
2023-03-27 22:00:05 -0400

    refactor: simplify logic for createExtent
    - refactor: simplify logic for createExtent + add imports from raster, sp, and sf + add helper functions

--- R/mapview_extent.R
+++ R/mapview_extent.R
@@ -1,38 +1,44 @@
 #' @keywords internal
+#' @importFrom raster extent projectExtent xmin xmax ymin ymax
+#' @importFrom sp CRS
+#' @importFrom sf st_bbox
 createExtent <- function(x, offset = NULL) {
-
-  if (inherits(x, "Extent")) {
+  if (is_extent(x)) {
     return(x)
-  } else {
-    if (inherits(x, "Raster")) {
-      ext <- raster::extent(
-        raster::projectExtent(x, crs = sp::CRS("+init=epsg:4326")))
-    } else if (inherits(x, "Spatial")) {
-      ext <- raster::extent(raster::xmin(x),
-                            raster::xmax(x),
-                            raster::ymin(x),
-                            raster::ymax(x))
-    } else if (inherits(x, "sfc") | inherits(x, "sf") |
-               inherits(x, "XY") | inherits(x, "stars")) {
-      bb <- sf::st_bbox(x)
-      ext <- raster::extent(bb[1], bb[3], bb[2], bb[4])
-    }
+  }
 
-    if (is.null(offset)) {
-      xxtend <- c(ext[1], ext[2])
-      yxtend <- c(ext[3], ext[4])
-      ext@xmin <- xxtend[1]
-      ext@xmax <- xxtend[2]
-      ext@ymin <- yxtend[1]
-      ext@ymax <- yxtend[2]
-    } else {
-      ext@xmin <- ext@xmin - offset
-      ext@xmax <- ext@xmax + offset
-      ext@ymin <- ext@ymin - offset
-      ext@ymax <- ext@ymax + offset
-    }
+  if (is_raster(x)) {
+    ext <- raster::extent(
+      raster::projectExtent(x, crs = sp::CRS("+init=epsg:4326"))
+    )
+  } else if (is_spatial(x)) {
+    ext <- raster::extent(
+      raster::xmin(x),
+      raster::xmax(x),
+      raster::ymin(x),
+      raster::ymax(x)
+    )
+  } else if (inherits(x, "sfc") | inherits(x, "sf") |
+    inherits(x, "XY") | inherits(x, "stars")) {
+    bb <- sf::st_bbox(x)
+    ext <- raster::extent(bb[1], bb[3], bb[2], bb[4])
+  }
+
+  if (is.null(offset)) {
+    xxtend <- c(ext[1], ext[2])
+    yxtend <- c(ext[3], ext[4])
+    ext@xmin <- xxtend[1]
+    ext@xmax <- xxtend[2]
+    ext@ymin <- yxtend[1]
+    ext@ymax <- yxtend[2]
 
     return(ext)
   }
 
+  ext@xmin <- ext@xmin - offset
+  ext@xmax <- ext@xmax + offset
+  ext@ymin <- ext@ymin - offset
+  ext@ymax <- ext@ymax + offset
+
+  ext
 }
