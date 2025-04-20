0235b4a6c1374fe2a93a63a6a6eb4e877daeb00f
SpatLyu <lyu.geosocial@gmail.com>
2025-04-21 00:58:42 +0800

    explicitly import %>% from magrittr

--- R/helpers.R
+++ R/helpers.R
@@ -8,3 +8,16 @@ insane_longitude_warning = function() {
             call. = FALSE)
   }
 }
+
+#' @title Pipe operator
+#' @description
+#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
+#' @name %>%
+#' @usage lhs \%>\% rhs
+#' @rdname pipe
+#' @keywords internal
+#' @importFrom magrittr %>%
+#' @return `NULL` (this is the magrittr pipe operator)
+#' @export
+NULL
