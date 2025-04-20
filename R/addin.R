fe22a1eb58f7b291184c96f4e0a1dcb310ccfc13
SpatLyu <lyu.geosocial@gmail.com>
2025-04-21 00:12:20 +0800

    refactor: replace @importFrom with explicit pkg::fun() calls in createFeatures

--- R/addin.R
+++ R/addin.R
@@ -1,6 +1,3 @@
-
-
-
 #' @title mapedit create features Addin
 #' @description Create and save spatial objects within the Rstudio IDE. Objects
 #' can then be saved to file types such as \code{.geojson} or \code{.shp}.
@@ -17,30 +14,22 @@
 #' @param SF_OBJECT sf Simple feature collection
 #'
 #' @return sf object and/or saved to file
-#' @importFrom miniUI miniPage miniContentPanel gadgetTitleBar miniButtonBlock
-#' @import shiny
-#' @importFrom shinyWidgets switchInput updateSwitchInput
-#' @importFrom mapview mapview
-#' @importFrom sf write_sf
-#' @importFrom dplyr bind_rows
-#' @importFrom leaflet setView
-#' @importFrom rstudioapi getActiveDocumentContext
 #' @export
 #'
 createFeatures <- function(SF_OBJECT = NULL) {
 
-  ui <- miniPage(
-    gadgetTitleBar("Edit Map"),
-    miniContentPanel(
-      editModUI("editor"),
-      miniButtonBlock(
-        div(style="display: inline-block;padding-top:22px;padding-left:30px;width:200px;",
-            switchInput('savefile', 'Save', value = FALSE, onStatus = "success", offStatus = "danger")),
-        div(style="display: inline-block; width: 400px;",
-            textInput('filename', '', value = 'saved_geometry.geojson')),
-        div(style="display: inline-block;padding-top:18px;width: 400px;font-size: 10pt;color: #313844;",
-            'The filename can include a path relative to working directory. ',
-            'A different file type can be selected by changing the file extension.')
+  ui <- miniUI::miniPage(
+    miniUI::gadgetTitleBar("Edit Map"),
+    miniUI::miniContentPanel(
+      mapedit::editModUI("editor"),
+      miniUI::miniButtonBlock(
+        htmltools::div(style="display: inline-block;padding-top:22px;padding-left:30px;width:200px;",
+                       shinyWidgets::switchInput('savefile', 'Save', value = FALSE, onStatus = "success", offStatus = "danger")),
+        htmltools::div(style="display: inline-block; width: 400px;",
+                       shiny::textInput('filename', '', value = 'saved_geometry.geojson')),
+        htmltools::div(style="display: inline-block;padding-top:18px;width: 400px;font-size: 10pt;color: #313844;",
+                       'The filename can include a path relative to working directory. ',
+                       'A different file type can be selected by changing the file extension.')
       )
     )
   )
@@ -63,28 +52,30 @@ createFeatures <- function(SF_OBJECT = NULL) {
     }, silent = TRUE)
 
     # update UI based on inputs
-    updateTextInput(session, 'filename', value = FILENAME)
+    shiny::updateTextInput(session, 'filename', value = FILENAME)
     if (FILENAME != 'saved_geometry.geojson') {
-      updateSwitchInput(session, 'savefile', value = TRUE)
+      shinyWidgets::updateSwitchInput(session, 'savefile', value = TRUE)
     }
 
     # load mapedit
     if ('sf' %in% class(SF_OBJECT)) {
-      geo <- callModule(editMod, "editor", mapview(SF_OBJECT)@map)
+      geo <- shiny::callModule(mapedit::editMod, "editor", mapview::mapview(SF_OBJECT)@map)
     } else {
-      geo <- callModule(editMod, "editor", setView(mapview()@map, 80, 0, 3))
+      geo <- shiny::callModule(mapedit::editMod, "editor", leaflet::setView(mapview::mapview()@map, 80, 0, 3))
     }
 
-    observe({
+    shiny::observe({
       input$filename
       OBJECTNAME <- tools::file_path_sans_ext(basename(input$filename))
     })
 
     # return geometry to file and object in console
-    observeEvent(input$done, {
+    shiny::observeEvent(input$done, {
       geom <- geo()$finished
 
-      if (!is.null(geom) & !is.null(SF_OBJECT)) geom <- dplyr::bind_rows(SF_OBJECT, geom)
+      if (!is.null(geom) & !is.null(SF_OBJECT)) {
+        geom <- dplyr::bind_rows(SF_OBJECT, geom)
+      }
 
       if (!is.null(geom)) {
         if (input$savefile) {
@@ -92,7 +83,7 @@ createFeatures <- function(SF_OBJECT = NULL) {
         }
       }
 
-      stopApp({
+      shiny::stopApp({
         if (!is.null(geom)) {
           geom
         }
@@ -101,7 +92,7 @@ createFeatures <- function(SF_OBJECT = NULL) {
 
   }
 
-  viewer <- paneViewer(600)
-  runGadget(ui, server, viewer = viewer)
+  viewer <- shiny::paneViewer(600)
+  shiny::runGadget(ui, server, viewer = viewer)
 
 }
